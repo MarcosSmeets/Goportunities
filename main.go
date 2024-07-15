33a949882a19dd0b41bcdd37ef0b425048df73b1
Marcos Smeets <48696729+MarcosSmeets@users.noreply.github.com>
2024-07-15 12:39:59 -0300

    add logs

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"github.com/MarcosSmeets/Goportunities/config"
 	"github.com/MarcosSmeets/Goportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.ErrorF("config initialization error: %v", err.Error())
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
